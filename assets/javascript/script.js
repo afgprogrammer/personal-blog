2015706f7d1f1edbf8f78213c5b086549c791fa2
Mohammad Rahmani <rto1680@gmail.com>
2017-12-25 15:53:59 +0330

    creare and design first page of website. [header, content, sidebar, footer]

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,33 @@
 /*****************
 *
-*    Theme Name: ''
-*    Theme URL: ''
+*    Theme Name: 'prsonal site'
+*    Theme URL: 'http://mohammadrahmani.com'
 *    Author: Mohammad Rahmani
 *    Author URL: http://mohammadrahmani.com
 *
 */
 
 $(document).ready(function () {
+    let scrollCount = 0;
+    let navbar_default = $('#header');
 
+    $(this).scroll(function () {
+        let scroll = $(this).scrollTop();
+        if ($(this).scrollTop() >= 400) {
+            navbar_default.css({
+                "background": "linear-gradient(120deg, #100D7E, #9E39FF)",
+                "transition": "all 5s"
+            });
+
+            if (scroll > scrollCount) {
+                navbar_default.css({ "top": "-100px" });
+            } else {
+                navbar_default.css({ "top": "0px" });
+            }
+
+            scrollCount = scroll;
+        } else if ($(this).scrollTop() <= 400) {
+            navbar_default.css({ "background": "rgba(255, 255, 255, 0.10)", "border": "1px solid rgba(255,255,255,0.03)"});
+        }
+    });
 });
